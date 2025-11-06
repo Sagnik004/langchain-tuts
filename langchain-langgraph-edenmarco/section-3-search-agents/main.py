8848f3f24f94f4d7276c32ee633f4a973d7e51b0
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2025-11-07 01:34:33 +0530

    section-3: Reliable Structured Output with LangChain's .with_structured_output()

--- langchain-langgraph-edenmarco/section-3-search-agents/main.py
+++ langchain-langgraph-edenmarco/section-3-search-agents/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
@@ -17,40 +17,42 @@ load_dotenv()
 # Define the tools
 tools = [TavilySearch()]
 
-# Define the llm
+# Define the llm, and provide it the Pydantic Agent Response format
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
+structured_llm = llm.with_structured_output(AgentResponse)
 
 # Pull the react prompt (special prompt template) from the hub
 react_prompt = hub.pull("hwchase17/react")
 
-# Output parser using Pydantic model
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+# Output parser using Pydantic model --> moved away to .with_structured_output()
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 
 # Make use of our customized ReAct template with ability to send expected output format instructions
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["tools", "tool_names", "input", "agent_scratchpad"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 # Create an agent that uses ReAct prompting (reasoning engine)
 agent = create_react_agent(
     llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
 
-# Agent runtime, extract the output coming from agent executor, parse the output into a Pydantic object
+# Agent runtime, extract the output coming from agent executor
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
 )
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
-chain = agent_executor | extract_output | parse_output
+# chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm      # structured_llm will convert the text output into Pydantic structured data
 
 
 def main():
     result = chain.invoke(
         input={
-            "input": "Search for 3 job postings for an AI engineer using LangChain in Hyderabad India on LinkedIn and list their details",
+            "input": "Search for 3 active job postings for an AI engineer using LangChain in Bangalore India on LinkedIn and list their details",
         }
     )
     print(result)
