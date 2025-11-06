d5222e24b10816c3da30b17f45831676a7133c25
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2025-11-06 12:36:05 +0530

    customized ReAct template with Pydantic object formatted output capability

--- langchain-langgraph-edenmarco/section-3-search-agents/main.py
+++ langchain-langgraph-edenmarco/section-3-search-agents/main.py
@@ -2,8 +2,14 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,21 +23,31 @@ llm = ChatOpenAI(model="gpt-4o", temperature=0)
 # Pull the react prompt (special prompt template) from the hub
 react_prompt = hub.pull("hwchase17/react")
 
+# Output parser using Pydantic model
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+# Make use of our customized ReAct template with ability to send expected output format instructions
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["tools", "tool_names", "input", "agent_scratchpad"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
 # Create an agent that uses ReAct prompting (reasoning engine)
 agent = create_react_agent(
-    llm=llm, 
-    tools=tools,
-    prompt=react_prompt
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
 
 # Agent runtime
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+)
 chain = agent_executor
+
 
 def main():
     result = chain.invoke(
         input={
-            "input": "Search for 3 job postings for an AI engineer using LangChain in Bangalore, India on LinkedIn and list their details",
+            "input": "Search for 3 job postings for an AI engineer using LangChain in Hyderabad India on LinkedIn and list their details",
         }
     )
     print(result)
