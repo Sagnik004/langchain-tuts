fdb4eaf38d23609dbaf093b7f4cf3552966f0130
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2025-11-09 14:11:47 +0530

    section-4: AgentAction, AgentFinish, ReAct AgentStep

--- langchain-langgraph-edenmarco/section-4-react-agents-deep-dive/main.py
+++ langchain-langgraph-edenmarco/section-4-react-agents-deep-dive/main.py
@@ -3,11 +3,12 @@ from langchain.tools import tool
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
 from langchain_core.tools.render import render_text_description
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_openai import ChatOpenAI
 from langchain_classic.agents.output_parsers.react_single_input import (
     ReActSingleInputOutputParser,
 )
-from langchain_core.agents import AgentAction, AgentFinish
+from langchain_classic.agents.format_scratchpad.log import format_log_to_str
 from typing import Union, List
 
 # Load environment variables from .env file
@@ -34,6 +35,7 @@ def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
             return tool
     raise ValueError(f"Tool with name {tool_name} not found!")
 
+
 def main():
     tools = [get_text_length]
 
@@ -57,7 +59,7 @@ Final Answer: the final answer to the original input question
 Begin!
 
 Question: {input}
-Thought:
+Thought:{agent_scratchpad}
 """
 
     prompt = PromptTemplate.from_template(template=template).partial(
@@ -72,29 +74,59 @@ Thought:
         # model_kwargs={"stop": ["\nObservation", "Observation", "Observation:"]}, # Gemini need this
     )
 
+    # Create a variable to keep track of the agent's scratchpad (its thoughts and observations)
+    intermediate_steps = []
+
     # Creating our agent. First it takes input, but instead of hardcoding we are passing a lambda so that when
     # the invoke method is called with a dictionary like {"input": "..."} we can extract that value and use it.
     # We then pass the input to prompt to generate a PromptTemplate object when then is passed to the LLM. And,
     # finally we parse the output with the ReActSingleInputOutputParser to extract the LLM response.
     agent = (
-        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
     )
 
     # Invoke the agent, and extract what tool to use and its input.
     # Sample response: tool='get_text_length' tool_input="'DOG'" log="To find the text length of 'DOG', I need to use the get_text_length tool.\n\nAction: get_text_length\nAction Input: 'DOG'"
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {"input": "What is the text length of 'DOG' in characters?"}
+    agent_step_1: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the text length of 'DOG' in characters?",
+            "agent_scratchpad": intermediate_steps,
+        }
     )
 
-    # If LLM response have a tool, tool_input, log etc. basically of type AgentAction, then go ahead and 
-    # call the tool after extracting out the tool name and input to be passed to it.
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
+    # If LLM response have a tool, tool_input, log etc. basically of type AgentAction, then go ahead and
+    # call the tool after extracting out the tool name and input to be passed to it. Capture the observation,
+    # and store it into intermediate_steps.
+    if isinstance(agent_step_1, AgentAction):
+        tool_name = agent_step_1.tool
         tool_to_use = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input
+        tool_input = agent_step_1.tool_input
 
         observation = tool_to_use.func(str(tool_input))
         print(f"Observation={observation}")
+        intermediate_steps.append((agent_step_1, str(observation)))
+
+    # Now, we can call the agent again with the updated intermediate_steps which now has the observation
+    # Sample response: return_values={'output': "The text length of 'DOG' in characters is 3."} log="I now know the final answer.\nFinal Answer: The text length of 'DOG' in characters is 3."
+    agent_step_2: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the text length of 'DOG' in characters?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+
+    # If we have the final response now which is of type AgentFinish, we can extract out the final output from it.
+    if isinstance(agent_step_2, AgentFinish):
+        print(f"Final Answer: {agent_step_2.return_values['output']}")
+
 
 if __name__ == "__main__":
     main()
+
+# QsK17%yrA*rv
