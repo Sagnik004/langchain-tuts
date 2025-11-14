bcb01c5f10e51ec9045944bb43a4e0e85eb4e7cf
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2025-11-14 11:58:50 +0530

    section-6: Medium Analyzer - Retrieval Implementation with LCEL

--- langchain-langgraph-edenmarco/section-6-rag-embeddings-vector-dbs/retrieval.py
+++ langchain-langgraph-edenmarco/section-6-rag-embeddings-vector-dbs/retrieval.py
@@ -5,12 +5,16 @@ from langchain_classic import hub
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 # Load environment variables from .env file
 load_dotenv()
 
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 def main():
     # Part-2: User query --> Embed user query --> Retrieve relevant vector embeddings from Pinecone --> Pass to LLM with user prompt and relevant chunks --> Answer
@@ -33,8 +37,28 @@ def main():
         retriever=vector_store.as_retriever(), combine_docs_chain=combine_docs_chain
     )
 
-    result = retrieval_chain.invoke(input={"input": query})
-    print(result)
+    # result = retrieval_chain.invoke(input={"input": query})
+    # print(result)
+
+    # A better prompt way, and implementation with LCEL
+    template = """Use the following pieces of context to answer the question at the end. If you don't find the answer, just respond with "I am not sure". Use three sentences maximum and keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Helpful Answer:
+    """
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": vector_store.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res.content)
 
 
 if __name__ == "__main__":
